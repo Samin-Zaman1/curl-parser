3156bfd2903ec1329a2b57439734bcb866673542
Rashiq Rahman <33522666+Rashiq50@users.noreply.github.com>
2024-12-15 20:32:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const Joi = require("joi");
 const converter = require("curl-to-postmanv2");
 
 const app = express();
+// need to use cors to allow call from any origin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
